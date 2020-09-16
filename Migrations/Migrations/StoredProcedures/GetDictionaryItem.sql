cab13bc98441df29361aa4e56cfaeb7cf83c157a
Reece Russell <reece@prominentmedia.com>
2020-09-16 21:59:09 +0100

    added dictionary items sprocs

---
+++ Migrations/Migrations/StoredProcedures/GetDictionaryItem.sql
@@ -0,0 +1,10 @@
+﻿CREATE OR ALTER PROCEDURE [dbo].GetDictionaryItem
+	@Id NVARCHAR(36)
+AS
+	SELECT
+		Id,
+		CultureName,
+		[Key],
+		[Value]
+	FROM DictionaryItems
+	WHERE Id = @Id;
---
+++ Migrations/Migrations/StoredProcedures/GetDictionaryItems.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR ALTER PROCEDURE [dbo].GetDictionaryItems
+	@CultureName NVARCHAR(14)
+AS
+	SELECT
+		Id,
+		CultureName,
+		[Key],
+		[Value],
+		DateCreated,
+		DateUpdated
+	FROM DictionaryItems
+	WHERE CultureName = @CultureName
+	ORDER BY [Key];
