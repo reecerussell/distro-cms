8fff69c12ef7eaa3b83bf3d3c2af4fe52348b269
Reece Russell <reece@prominentmedia.com>
2020-09-21 10:32:57 +0100

    Custom shared configuration method

---
+++ Shared/Shared/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace Shared.Extensions
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder ConfigureSharedConfiguration(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder.ConfigureAppConfiguration((builder, config) =>
+            {
+                config.AddSettings(builder.HostingEnvironment, "appsettings");
+                config.AddSettings(builder.HostingEnvironment, "settings");
+            });
+        }
+
+        private static void AddSettings(this IConfigurationBuilder builder, IHostEnvironment environment, string filename)
+        {
+            if (environment.IsDevelopment())
+            {
+                filename += ".development.json";
+            }
+            else
+            {
+                filename += ".json";
+            }
+
+            if (!File.Exists(filename))
+            {
+                return;
+            }
+
+            builder.AddJsonFile(filename);
+        }
+    }
+}
