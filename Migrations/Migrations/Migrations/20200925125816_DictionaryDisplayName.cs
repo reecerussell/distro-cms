2f4288bf8fa2ab5f1ed050aac0238b855e3a5f5b
Reece Russell <reece@prominentmedia.com>
2020-09-25 14:01:26 +0100

    added display name migration

---
+++ Migrations/Migrations/Migrations/20200925125816_DictionaryDisplayName.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migrations.Migrations
+{
+    public partial class DictionaryDisplayName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "DictionaryItems",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "DictionaryItems");
+        }
+    }
+}
