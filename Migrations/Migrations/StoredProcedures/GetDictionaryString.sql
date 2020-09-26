a0b96dee4e00006f3e260f74a951983dfd029aa6
Reece Russell <reece@prominentmedia.com>
2020-09-26 18:02:32 +0100

    added a sproc to get a culture specific string

---
+++ Migrations/Migrations/StoredProcedures/GetDictionaryString.sql
@@ -0,0 +1,8 @@
+﻿CREATE OR ALTER PROCEDURE [dbo].GetDictionaryString
+	@Key NVARCHAR(45),
+	@CultureName NVARCHAR(14)
+AS
+	SELECT [Value] 
+	FROM DictionaryItems 
+	WHERE [Key] = @Key
+		AND CultureName = @CultureName;
