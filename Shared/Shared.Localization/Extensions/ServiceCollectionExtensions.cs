50101f4555d823cb388d742ebccb98714299cf3f
Reece Russell <reece@prominentmedia.com>
2020-09-26 18:05:31 +0100

    added a localization lib

---
+++ Shared/Shared.Localization/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Localization.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddLocalizer(this IServiceCollection services)
+        {
+            return services.AddTransient<ILocalizer, Localizer>();
+        }
+    }
+}
