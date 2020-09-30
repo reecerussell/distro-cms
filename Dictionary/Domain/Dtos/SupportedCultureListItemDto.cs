533d761f74dec4c86b19b0efd0cea045b01127e5
Reece Russell <reece@prominentmedia.com>
2020-09-30 10:45:36 +0100

    created a dto for the culture list

---
+++ Dictionary/Domain/Dtos/SupportedCultureListItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Dictionary.Domain.Dtos
+{
+    public class SupportedCultureListItemDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
