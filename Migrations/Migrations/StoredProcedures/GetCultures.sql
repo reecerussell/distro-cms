89083c1a50b52c6b855a9dc82600a31d1937cca4
Reece Russell <reece@prominentmedia.com>
2020-09-30 10:46:40 +0100

    created a stored procedure to get a list of supported cultures

---
+++ Migrations/Migrations/StoredProcedures/GetCultures.sql
@@ -0,0 +1,19 @@
+﻿CREATE OR ALTER PROCEDURE [dbo].GetCultures
+	@SearchTerm NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @query NVARCHAR(MAX);
+	SET @query = 'SELECT 
+					[Id], 
+					[Name], 
+					[DisplayName], 
+					[IsDefault]
+				  FROM SupportedCultures ';
+
+	IF @SearchTerm IS NOT NULL
+		SET @query = @query + 'WHERE ([Name] LIKE @SearchTerm OR [DisplayName] LIKE @SearchTerm) ';
+
+	SET @query = @query + 'ORDER BY [IsDefault] DESC, [DisplayName];';
+
+	EXEC sp_executesql @query, N'@SearchTerm NVARCHAR(MAX)', @SearchTerm;
+END
