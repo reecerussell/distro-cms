1c71e72d04714036781600065ddee951b6644ce3
Reece Russell <reece@prominentmedia.com>
2020-10-02 13:36:10 +0100

    added another ResponseData class with a type parameter

--- Shared/Shared/Controllers/ResponseData.cs
+++ Shared/Shared/Controllers/ResponseData.cs
@@ -6,4 +6,11 @@
         public object Data { get; set; }
         public string Error { get; set; }
     }
+
+    public class ResponseData<T> where T : class
+    {
+        public int StatusCode { get; set; }
+        public T Data { get; set; }
+        public string Error { get; set; }
+    }
 }
