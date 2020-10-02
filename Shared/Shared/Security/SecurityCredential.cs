e3edf7d0785044316796108f147b65249b641a77
Reece Russell <reece@prominentmedia.com>
2020-10-02 13:36:47 +0100

    added a collection of common security based objects

---
+++ Shared/Shared/Security/SecurityCredential.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared.Security
+{
+    /// <summary>
+    /// An object containing credential properties for OAuth.
+    /// </summary>
+    public class SecurityCredential
+    {
+        public string GrantType { get; set; }
+        public string Audience { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        // TODO: support refresh token and auth code grant types.
+        //public string RefreshToken { get; set; }
+        //public string AuthCode { get; set; }
+    }
+}
---
+++ Shared/Shared/Security/Token.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Security
+{
+    public class Token
+    {
+        public string AccessToken { get; set; }
+        public double Expires { get; set; }
+    }
+}
