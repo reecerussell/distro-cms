a09436b2d9fe1f4f1996668a8c3089887b951754
Reece Russell <reece@prominentmedia.com>
2020-10-05 15:43:50 +0100

    Added a claim dto to transfer claims

---
+++ Shared/Shared/Security/ClaimDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared.Security
+{
+    public class ClaimDto
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public ClaimDto()
+        {
+        }
+
+        public ClaimDto(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
