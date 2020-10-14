1aee72da1497d0cde1edbc12868da3b1a1bf9a64
Reece Russell <reece@prominentmedia.com>
2020-10-14 11:05:40 +0100

    Added toast service to display alerts on creation of an item

--- cms/src/app/api/dictionary.service.ts
+++ cms/src/app/api/dictionary.service.ts
@@ -5,12 +5,13 @@ import { map } from "rxjs/operators";
 import DictionaryItem from "../models/dictionary-item.model";
 import { environment } from "../../environments/environment";
 import { ResponseBody } from "./responses";
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
     providedIn: "root",
 })
 export class DictionaryService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private toastr: ToastrService) {}
 
     GetList$(culture: string): Observable<DictionaryItem[]> {
         const options = {
@@ -49,10 +50,12 @@ export class DictionaryService {
                 map((responseBody: ResponseBody) => {
                     const { statusCode, data, error } = responseBody;
                     if (statusCode === 200) {
+                        this.toastr.success("Successfully added a dictionary value.")
                         return data as DictionaryItem;
                     }
 
-                    throw new Error(error);
+                    this.toastr.error(error)
+                    throw new Error();
                 })
             );
     }
