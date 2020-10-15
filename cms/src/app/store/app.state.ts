caa9bc0023307c7b7f9a2e0fbca5722995f0e5ec
Reece Russell <reece@prominentmedia.com>
2020-10-15 11:32:51 +0100

    Added the users state to the app state model

--- cms/src/app/store/app.state.ts
+++ cms/src/app/store/app.state.ts
@@ -1,7 +1,9 @@
 import { DictionaryState } from "./dictionary/dictionary.state";
 import { RoleListState } from "./roles/role.state";
+import { UsersState } from "./users/user.state";
 
 export default interface AppState {
     roles: RoleListState;
     dictionary: DictionaryState;
+    users: UsersState;
 }
