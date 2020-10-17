d24fcb2a125da10f7bf31e9fb676c97a2c6bed48
Reece Russell <reece@prominentmedia.com>
2020-10-17 18:38:32 +0100

    Added basic unit tests for the password generator

---
+++ Shared/Shared.Tests/Passwords/GeneratorTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.Options;
+using Shared.Passwords;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Shared.Tests.Passwords
+{
+    public class GeneratorTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public GeneratorTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void TestConstructorWithNullOptions()
+        {
+            Assert.Throws<ArgumentNullException>(() => new PasswordGenerator(null));
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var options = Options.Create<PasswordOptions>(null);
+                _ = new PasswordGenerator(options);
+            });
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var options = Options.Create(new PasswordOptions {Generator = null});
+                _ = new PasswordGenerator(options);
+            });
+        }
+
+        [Fact]
+        public void TestConstructorWithNullLowercaseChars()
+        {
+            var options = Options.Create(new PasswordOptions {Generator = {LowercaseCharacters = null}});
+
+            var ex = Assert.Throws<ArgumentNullException>(() => new PasswordGenerator(options));
+            Assert.Contains("LowercaseCharacters", ex.Message);
+        }
+
+        [Fact]
+        public void TestConstructorWithNullUppercaseChars()
+        {
+            var options = Options.Create(new PasswordOptions { Generator = { UppercaseCharacters = null } });
+
+            var ex = Assert.Throws<ArgumentNullException>(() => new PasswordGenerator(options));
+            Assert.Contains("UppercaseCharacters", ex.Message);
+        }
+
+        [Fact]
+        public void TestConstructorWithNullDigits()
+        {
+            var options = Options.Create(new PasswordOptions { Generator = { Digits = null } });
+
+            var ex = Assert.Throws<ArgumentNullException>(() => new PasswordGenerator(options));
+            Assert.Contains("Digits", ex.Message);
+        }
+
+        [Fact]
+        public void TestConstructorWithNullSpecialChars()
+        {
+            var options = Options.Create(new PasswordOptions { Generator = { SpecialCharacters = null } });
+
+            var ex = Assert.Throws<ArgumentNullException>(() => new PasswordGenerator(options));
+            Assert.Contains("SpecialCharacters", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(12)]
+        [InlineData(32)]
+        public void TestGenerate(int length)
+        {
+            var generator = new PasswordGenerator(Options.Create(new PasswordOptions()));
+            var password = generator.Generate(length);
+            Assert.Equal(length, password.Length);
+
+            _testOutputHelper.WriteLine(password);
+        }
+    }
+}
