93c61f7d722977301959ef7121bd426fc3993f25
Reece Russell <reece@prominentmedia.com>
2020-10-17 18:35:06 +0100

    Added a new options class for a password generator

---
+++ Shared/Shared/Passwords/PasswordGeneratorOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Passwords
+{
+    public class PasswordGeneratorOptions
+    {
+        public string UppercaseCharacters { get; set; } = "abcdefghijklmnopqrstuvwxyz";
+        public string LowercaseCharacters { get; set; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public string Digits { get; set; } = "0123456789";
+        public string SpecialCharacters { get; set; } = "-_#@$*";
+    }
+}
--- Shared/Shared/Passwords/PasswordOptions.cs
+++ Shared/Shared/Passwords/PasswordOptions.cs
@@ -4,11 +4,13 @@
     {
         public PasswordHasherOptions Hasher { get; set; }
         public PasswordValidationOptions Validation { get; set; }
+        public PasswordGeneratorOptions Generator { get; set; }
 
         public PasswordOptions()
         {
             Hasher = new PasswordHasherOptions();
             Validation = new PasswordValidationOptions();
+            Generator = new PasswordGeneratorOptions();
         }
     }
 }
