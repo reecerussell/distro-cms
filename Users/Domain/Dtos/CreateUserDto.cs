3ddff9280b5451e85d39c5e644e493e9acbba4b2
Reece Russell <reece@prominentmedia.com>
2020-10-18 13:08:14 +0100

    Removed the password field as this will now be randomly generated

--- Users/Domain/Dtos/CreateUserDto.cs
+++ Users/Domain/Dtos/CreateUserDto.cs
@@ -5,6 +5,5 @@
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
